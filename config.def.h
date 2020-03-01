cdfde2b3dee4ced5c012f0b7ccd546ce5348bee2
Fadi EL HADDAD <fadi.haddad.dev@gmail.com>
2020-03-01 16:35:41 +0000

    adding following copy url st-copyurl-20190202-3be4cf1.diff

--- config.def.h
+++ config.def.h
@@ -220,6 +220,7 @@ static Shortcut shortcuts[] = {
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ TERMMOD,              XK_Return,      newterm,        {.i =  0} },
+	{ MODKEY,               XK_l,           copyurl,        {.i =  0} },
 };
 
 /*
